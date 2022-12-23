23f0ba44640e438123134f7d3ea641002e576985
tosha504~ <you@example.com>
2022-12-23 13:59:16 +0100

    js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+$(document).ready(function() { 
 const header = $('header'),
   burger = $(".header__burger span"),
   body = $("body"),
@@ -8,3 +9,36 @@ const header = $('header'),
     nav.toggleClass("active");
     body.toggleClass("fixed-page");
   }); 
+
+  // console.log($("a[href^='#mission']"));
+const mission = document.querySelector('#mission'),
+about = document.querySelector('#about'),
+contact = document.querySelector('#contact');
+
+const options = {
+  threshold: [1]
+}
+
+
+const callback = function (entries, observer) {
+  entries.forEach( entry => {
+    const {mission, isIntersecting, intersectionRatio} = entry;
+    if(isIntersecting) {
+      // console.log('da');
+      console.log(this);
+      $("a[href^='#mission']").addClass('active');
+    }else {
+      // console.log('net');
+      $("a[href^='#mission']").removeClass('active');
+    }
+  });
+}
+let observer = new IntersectionObserver(callback, options);
+
+observer.observe(mission);
+observer.observe(about);
+
+// console.log(observer);
+// console.log(mission);
+
+})
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,41 @@
-var header = $('header'),
-  burger = $(".header__burger span"),
-  body = $("body"),
-  nav = $(".header__nav");
-burger.on("click", function () {
-  burger.toggleClass("active");
-  nav.toggleClass("active");
-  body.toggleClass("fixed-page");
+$(document).ready(function () {
+  var header = $('header'),
+    burger = $(".header__burger span"),
+    body = $("body"),
+    nav = $(".header__nav");
+  burger.on("click", function () {
+    burger.toggleClass("active");
+    nav.toggleClass("active");
+    body.toggleClass("fixed-page");
+  });
+
+  // console.log($("a[href^='#mission']"));
+  var mission = document.querySelector('#mission'),
+    about = document.querySelector('#about'),
+    contact = document.querySelector('#contact');
+  var options = {
+    threshold: [1]
+  };
+  var callback = function callback(entries, observer) {
+    var _this = this;
+    entries.forEach(function (entry) {
+      var mission = entry.mission,
+        isIntersecting = entry.isIntersecting,
+        intersectionRatio = entry.intersectionRatio;
+      if (isIntersecting) {
+        // console.log('da');
+        console.log(_this);
+        $("a[href^='#mission']").addClass('active');
+      } else {
+        // console.log('net');
+        $("a[href^='#mission']").removeClass('active');
+      }
+    });
+  };
+  var observer = new IntersectionObserver(callback, options);
+  observer.observe(mission);
+  observer.observe(about);
+
+  // console.log(observer);
+  // console.log(mission);
 });
