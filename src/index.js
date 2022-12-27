5f0993ef30207814506fbbbd2ce7a848bcb59168
tosha504~ <you@example.com>
2022-12-27 14:57:49 +0100

    pre last

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
 const header = $('header'),
   burger = $(".header__burger span"),
   body = $("body"),
-  nav = $(".header__nav");
+  nav = $(".header__nav"),
+  mission = document.querySelector('#mission'),
+  about = document.querySelector('#about'),
+  contact = document.querySelector('#contact');
+ 
 
   burger.on("click", function() {
     burger.toggleClass("active");
@@ -10,35 +14,89 @@ const header = $('header'),
     body.toggleClass("fixed-page");
   }); 
 
-  // console.log($("a[href^='#mission']"));
-const mission = document.querySelector('#mission'),
-about = document.querySelector('#about'),
-contact = document.querySelector('#contact');
-
-const options = {
-  threshold: [1]
-}
-
-
-const callback = function (entries, observer) {
-  entries.forEach( entry => {
-    const {mission, isIntersecting, intersectionRatio} = entry;
-    if(isIntersecting) {
-      // console.log('da');
-      console.log(this);
-      $("a[href^='#mission']").addClass('active');
-    }else {
-      // console.log('net');
-      $("a[href^='#mission']").removeClass('active');
-    }
+  $("a[href^='#']").on("click", function(e) {
+    const target = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(target).offset().top - 90 }, 1000);       
   });
-}
-let observer = new IntersectionObserver(callback, options);
+                           
 
-observer.observe(mission);
-observer.observe(about);
+    
+ 
+  let options = { }
+  function opt() {
+    if($( window ).width() > 1400) {
+      options = {
+        threshold: 0.9
+      }
+    } 
+    if($( window ).width() < 1400) {
+      options = {
+        threshold: 0.8
+      }
+    }
+    if($( window ).width() < 1005) {
+      options = {
+        threshold: 0.3
+      }
+    }
 
-// console.log(observer);
-// console.log(mission);
+  }
 
-})
+  opt();
+  $( window ).resize(function() {
+    opt();
+  })
+  
+  let arr = [mission, about, contact]
+
+  const callback = function (entries, observer) {
+    entries.forEach( entry => {
+      const { isIntersecting, intersectionRatio} = entry;
+      if(isIntersecting ) {
+        // console.log(entry);
+        $(`a[href^='#${entry.target.id}']`).addClass('active');
+      }else {
+        $(`a[href^='#${entry.target.id}']`).removeClass('active');
+      }
+    });
+  }
+
+  let observer = new IntersectionObserver(callback, options);
+  arr.forEach(e => {
+    observer.observe(e);
+  })
+
+   //check to see if the submited cookie is set, if not check if the popup has been closed, if not then display the popup
+   if( getCookie('popupCookie') != 'submited'){ 
+    $('.cookies').css("display", "block").hide().fadeIn(2000);
+    }
+
+    $('a.submit').click(function(){
+    $('.cookies').fadeOut();
+    //sets the coookie to five minutes if the popup is submited (whole numbers = days)
+    setCookie( 'popupCookie', 'submited', 7 );
+    });
+
+    function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+
+    function setCookie(cname, cvalue, exdays) {
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+      var expires = "expires=" + d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+  
+}); 
--- src/js/index.js
+++ src/js/index.js
@@ -2,40 +2,88 @@ $(document).ready(function () {
   var header = $('header'),
     burger = $(".header__burger span"),
     body = $("body"),
-    nav = $(".header__nav");
+    nav = $(".header__nav"),
+    mission = document.querySelector('#mission'),
+    about = document.querySelector('#about'),
+    contact = document.querySelector('#contact');
   burger.on("click", function () {
     burger.toggleClass("active");
     nav.toggleClass("active");
     body.toggleClass("fixed-page");
   });
-
-  // console.log($("a[href^='#mission']"));
-  var mission = document.querySelector('#mission'),
-    about = document.querySelector('#about'),
-    contact = document.querySelector('#contact');
-  var options = {
-    threshold: [1]
-  };
+  $("a[href^='#']").on("click", function (e) {
+    var target = $(this).attr("href");
+    $("html, body").animate({
+      scrollTop: $(target).offset().top - 90
+    }, 1000);
+  });
+  var options = {};
+  function opt() {
+    if ($(window).width() > 1400) {
+      options = {
+        threshold: 0.9
+      };
+    }
+    if ($(window).width() < 1400) {
+      options = {
+        threshold: 0.8
+      };
+    }
+    if ($(window).width() < 1005) {
+      options = {
+        threshold: 0.3
+      };
+    }
+  }
+  opt();
+  $(window).resize(function () {
+    opt();
+  });
+  var arr = [mission, about, contact];
   var callback = function callback(entries, observer) {
-    var _this = this;
     entries.forEach(function (entry) {
-      var mission = entry.mission,
-        isIntersecting = entry.isIntersecting,
+      var isIntersecting = entry.isIntersecting,
         intersectionRatio = entry.intersectionRatio;
       if (isIntersecting) {
-        // console.log('da');
-        console.log(_this);
-        $("a[href^='#mission']").addClass('active');
+        // console.log(entry);
+        $("a[href^='#".concat(entry.target.id, "']")).addClass('active');
       } else {
-        // console.log('net');
-        $("a[href^='#mission']").removeClass('active');
+        $("a[href^='#".concat(entry.target.id, "']")).removeClass('active');
       }
     });
   };
   var observer = new IntersectionObserver(callback, options);
-  observer.observe(mission);
-  observer.observe(about);
+  arr.forEach(function (e) {
+    observer.observe(e);
+  });
 
-  // console.log(observer);
-  // console.log(mission);
+  //check to see if the submited cookie is set, if not check if the popup has been closed, if not then display the popup
+  if (getCookie('popupCookie') != 'submited') {
+    $('.cookies').css("display", "block").hide().fadeIn(2000);
+  }
+  $('a.submit').click(function () {
+    $('.cookies').fadeOut();
+    //sets the coookie to five minutes if the popup is submited (whole numbers = days)
+    setCookie('popupCookie', 'submited', 7);
+  });
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
 });
